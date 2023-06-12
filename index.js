96f7a57b7c82d587c2863cf7e7d41f0dbde378d8
billal-fahmid <billalcooom@gmail.com>
2023-06-12 22:53:52 +0600

    create verify admin

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const jwt =require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -17,17 +17,17 @@ app.use(express.json())
 
 
 
-const verifyJWT=(req, res, next) =>{
-  const authorization=req.headers.authorization;
-  if(!authorization){
-    return res.status(401).send({error:true , message:'Unauthorized Access'})
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'Unauthorized Access' })
   }
 
-  const token= authorization.split(' ')[1]
+  const token = authorization.split(' ')[1]
 
-  jwt.verify(token , process.env.ACCESS_TOKEN_SECRET , (err, decoded) =>{
-    if(err){
-      return res.status(401).send({error:true , message:'Unauthorized Access'})
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'Unauthorized Access' })
     }
     req.decoded = decoded;
     next();
@@ -58,34 +58,63 @@ async function run() {
     const classesCollection = client.db('fsaDb').collection('classes');
     const selectClassCollection = client.db('fsaDb').collection('selectClass');
 
-   
-    app.post('/jwt' , (req,res )=>{
+
+    app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'7d'})
-      res.send({token})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' })
+      res.send({ token })
     })
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+
+      const user = await usersCollection.findOne(query)
+      if (user?.status !== 'admin') {
+          return res.status(403).send({ error: true, message: 'forbidden message' })
+      }
+      next()
+  }
 
 
 
 
     // save user info in db
 
-    app.put('/users/:email' ,  async (req, res) =>{
-        const email = req.params.email;
-        const user = req.body;
-        const query = {email: email}
-        const options = {
-            upsert:true
-        }
-        const updatedDoc={
-            $set:user
-        }
-        const result =await usersCollection.updateOne(query,updatedDoc,options)
-        console.log(result);
-        res.send(result)
+    app.put('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = { email: email }
+      const options = {
+        upsert: true
+      }
+      const updatedDoc = {
+        $set: user
+      }
+      const result = await usersCollection.updateOne(query, updatedDoc, options)
+      console.log(result);
+      res.send(result)
     })
 
-    app.get('/users' , verifyJWT, async(req ,res) =>{
+
+
+    app.get('/users/admin/:email' , verifyJWT , async (req,res) =>{
+      const email = req.params.email;
+      if(req.decoded.email !== email){
+        return res.send({admin:false})
+      }
+
+      const query = {email: email}
+      const user = await usersCollection.findOne(query)
+      // console.log(user)
+      const result =  user?.status 
+      // console.log({result})
+      res.send(result)
+    })
+
+
+
+    app.get('/users', verifyJWT,verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray()
       res.send(result)
     })
@@ -94,41 +123,68 @@ async function run() {
 
 
     // classes api
-    app.get('/classes' , async (req,res) =>{
-        const result = await classesCollection.find().toArray()
-        res.send(result)
-    })
-
-    app.post('/classes' , verifyJWT,async(req,res) =>{
-      const addClass = req.body;
-      const result =await classesCollection.insertOne(addClass)
+    app.get('/classes', async (req, res) => {
+      const result = await classesCollection.find().toArray()
       res.send(result)
     })
 
 
-   app.post('/selectedclasses' , async (req, res) =>{
-      const cla =req.body
-      
+
+    app.post('/selectedclasses', async (req, res) => {
+      const cla = req.body
+
       const result = await selectClassCollection.insertOne(cla)
       res.send(result)
-   }) 
-   
-   app.get('/selectedclasses',verifyJWT, async (req,res)=>{
+    })
+
+    app.get('/selectedclasses', verifyJWT, async (req, res) => {
       const email = req.query.email;
-      if(!email){
+      if (!email) {
         return res.send([])
       }
-      const query = {email : email};
+
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'Forbidden Access' })
+      }
+
+      const query = { email: email };
       const result = await selectClassCollection.find(query).toArray()
       res.send(result)
-   })
+    })
 
-   app.delete('/selectedclasses/:id', async (req, res) =>{
+    app.delete('/selectedclasses/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await selectClassCollection.deleteOne(query);
       res.send(result)
-   })
+    })
+
+    // add class for instructor
+    app.post('/classes', verifyJWT, async (req, res) => {
+      const addClass = req.body;
+      const result = await classesCollection.insertOne(addClass)
+      res.send(result)
+    })
+
+    //  myclasses for instructor 
+
+    app.get('/myclasses', verifyJWT, async (req, res) => {
+      const email = req.query.email
+      // console.log(email)
+      if (!email) {
+        return res.send([])
+      }
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'Forbidden Access' })
+      }
+      const query = { instructorEmail: email }
+      const result = await classesCollection.find(query).toArray()
+      res.send(result)
+
+    })
+
 
 
 
